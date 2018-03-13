f77612495603a6b80abbc46a7e20ee274797b5ae
ShizaSakei <ortegacesar06@gmail.com>
2018-03-12 22:25:43 -0500

    Prueba 1

---
+++ Programacion/Prueba 1/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Deus Vult
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int e = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de estudiantes: "));
+        int m = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de materias: "));
+        
+        Estudiantes clase = new Estudiantes(e,m);
+        
+        for (int i = 0; i < e; i++) {
+            clase.setEstudiante(i, JOptionPane.showInputDialog("Ingrese el nombre del estudiante #"+(i+1)+": "));
+        }
+        
+        for (int i = 0; i < m; i++) {
+            clase.setMateria(i, JOptionPane.showInputDialog("Ingrese el nombre de la materia #"+(i+1)+": ")); 
+        }
+        
+        JOptionPane.showMessageDialog(null, clase.presentar());
+    }
+    
+}
